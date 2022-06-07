f7e96f4c3917d96e579008c33156e7fcc1c72703
HEEKWON <higun295@gmail.com>
2022-06-07 21:02:09 +0900

    20220607_3

--- 10804/Program.cs
+++ 10804/Program.cs
@@ -6,6 +6,30 @@ namespace _10804
     {
         static void Main (string[] args)
         {
+            int[] array = new int[21];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                string[] input = Console.ReadLine().Split(' ');
+                int A = int.Parse(input[0]);
+                int B = int.Parse(input[1]);
+
+                for (int j = 0; j < (B - A + 1) / 2; j++)
+                {
+                    int temp = array[A + j];
+                    array[A + j] = array[B - j];
+                    array[B - j] = temp;
+                }
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
         }
     }
 }
---
+++ 15552/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _15552
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+            StringBuilder sb = new StringBuilder();
+
+            int T = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < T; i++)
+            {
+                string[] input = sr.ReadLine().Split(' ');
+                int A = int.Parse(input[0]);
+                int B = int.Parse(input[1]);
+
+                sb.Append(A + B).Append("\n");
+            }
+
+            sw.WriteLine(sb);
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
