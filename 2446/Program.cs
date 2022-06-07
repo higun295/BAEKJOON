8ed09dd352a4fad31fdd9e71cc798c07b6d61abe
HEEKWON <higun295@gmail.com>
2022-06-08 00:10:25 +0900

    20220608_1

---
+++ 2446/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace _2446
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < N - 1; i++)
+            {
+                for (int j = 0; j < i; j++)
+                    sb.Append(" ");
+                for (int j = 0; j < 2 * (N - i) - 1; j++)
+                    sb.Append("*");
+
+                sb.Append("\n");
+            }
+
+            for (int i = 0; i < N - 1; i++)
+                sb.Append(" ");
+
+            sb.Append("*");
+            sb.Append("\n");
+
+            for (int i = 0; i < N - 1; i++)
+            {
+                for (int j = 0; j < N - i - 2; j++)
+                    sb.Append(" ");
+                for (int j = 0; j < 2 * (i + 1) + 1; j++)
+                    sb.Append("*");
+
+                sb.Append("\n");
+            }
+
+            Console.Write(sb);
+        }
+    }
+}
---
+++ 2562/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _2562
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int max = -1;
+            int index = 0;
+
+            for (int i = 0; i < 9; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+                if (input > max)
+                {
+                    max = input;
+                    index = i + 1;
+                }
+            }
+
+            Console.WriteLine(max);
+            Console.WriteLine(index);
+        }
+    }
+}
