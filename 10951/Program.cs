c7601cb084cadccad5a6b2d0319367aad26c492f
HEEKWON <higun295@gmail.com>
2022-06-13 23:53:15 +0900

    20220613_1

---
+++ 10951/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _10951
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+            StringBuilder sb = new StringBuilder();
+
+            while (sr.EndOfStream == false)
+            {
+                string[] inputArray = sr.ReadLine().Split(' ');
+                int A = int.Parse(inputArray[0]);
+                int B = int.Parse(inputArray[1]);
+
+                sb.Append((A + B));
+                sb.Append("\n");
+            }
+
+            Console.Write(sb);
+            sr.Close();
+        }
+    }
+}
---
+++ 2525/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _2525
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            int A = int.Parse(input[0]);
+            int B = int.Parse(input[1]);
+
+            int input2 = int.Parse(Console.ReadLine());
+            int totalMinutes = 60 * A + B;
+            int result = totalMinutes + input2;
+
+            A = result / 60;
+            B = result % 60;
+
+            if (A >= 24)
+                A -= 24;
+
+            Console.WriteLine($"{A} {B}");
+        }
+    }
+}
