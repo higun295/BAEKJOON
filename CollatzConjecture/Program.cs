56bc5740e6f66fd70c964297f099bd35f0fea687
HEEKWON <higun295@gmail.com>
2022-06-16 22:24:48 +0900

    20220616_1

---
+++ CollatzConjecture/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CollatzConjecture
+{
+    internal class Program
+    {
+        static void Main (string[] args)
+        {
+            long input = long.Parse(Console.ReadLine());
+            long count = 0;
+
+            while (true)
+            {
+                if (input == 1)
+                    break;
+
+                if (input % 2 == 0)
+                    input = input / 2;
+                else
+                    input = 3 * input + 1;
+
+                count++;
+                Console.WriteLine($"Current : {input}({count})");
+            }
+        }
+    }
+}
