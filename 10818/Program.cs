7e9181c0b982ab17c343d6b1c5944de63f0bf6b3
HEEKWON <higun295@gmail.com>
2022-06-30 00:27:15 +0900

    20220630_1

---
+++ 10818/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _10818
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+            Array.Sort(array);
+            Console.WriteLine($"{array[0]} {array[N - 1]}");
+        }
+    }
+}
