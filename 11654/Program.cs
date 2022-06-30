db21b105f7b9fbf827d75178b727eeb0eda5443a
HEEKWON <higun295@gmail.com>
2022-06-30 11:25:28 +0900

    20220630_2

---
+++ 11654/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _11654
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char input = char.Parse(Console.ReadLine());
+            Console.WriteLine(Convert.ToInt32(input));
+        }
+    }
+}
